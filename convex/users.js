f801b19814c5cc378c5adcb6674c844ece27d218
SankThomas <thomassankara74@gmail.com>
2026-01-13 21:52:54 +0300

    add actions to send clerk invites to users

--- convex/users.js
+++ convex/users.js
@@ -1,4 +1,4 @@
-import { query, mutation } from "./_generated/server";
+import { query, mutation, action } from "./_generated/server";
 import { v } from "convex/values";
 
 export const getCurrentUser = query({
@@ -18,7 +18,7 @@ export const createUser = mutation({
     email: v.string(),
     name: v.string(),
     role: v.optional(
-      v.union(v.literal("user"), v.literal("agent"), v.literal("admin"))
+      v.union(v.literal("user"), v.literal("agent"), v.literal("admin")),
     ),
   },
   handler: async (ctx, { clerkId, email, name, role = "user" }) => {
@@ -58,7 +58,7 @@ export const getAllAgents = query({
     const agents = await ctx.db
       .query("users")
       .filter((q) =>
-        q.or(q.eq(q.field("role"), "agent"), q.eq(q.field("role"), "admin"))
+        q.or(q.eq(q.field("role"), "agent"), q.eq(q.field("role"), "admin")),
       )
       .collect();
     return agents;
@@ -69,5 +69,68 @@ export const getAllUsers = query({
   handler: async (ctx) => {
     const users = await ctx.db.query("users").order("desc").collect();
     return users;
+  },
+});
+
+// Handle user invites
+export const inviteUser = action({
+  args: {
+    email: v.string(),
+    role: v.union(v.literal("user"), v.literal("agent"), v.literal("admin")),
+  },
+  handler: async (_, args) => {
+    const clerkSecret = "sk_test_dS0czaBtwWxQvz5584VfEJqDbMt4oAxq1F0QMQr2SC";
+
+    if (!clerkSecret) {
+      throw new Error("Missing Clerk Secret Key");
+    }
+
+    const res = await fetch("https://api.clerk.com/v1/invitations", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${clerkSecret}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email_address: args.email,
+        public_metadata: { role: args.role },
+      }),
+    });
+
+    if (!res.ok) {
+      const error = await res.text();
+      throw new Error(error);
+    }
+  },
+});
+
+export const upsertUserFromClerk = mutation({
+  args: {
+    clerkId: v.string(),
+    email: v.string(),
+    name: v.string(),
+    role: v.union(v.literal("user"), v.literal("agent"), v.literal("admin")),
+  },
+  handler: async (ctx, args) => {
+    const existingUser = await ctx.db
+      .query("users")
+      .withIndex("by_clerk_id", (q) => q.eq("clerkId", args.clerkId))
+      .first();
+
+    if (existingUser) {
+      await ctx.db.patch(existingUser._id, {
+        role: args.role,
+      });
+
+      return;
+    }
+
+    await ctx.db.insert("users", {
+      clerkId: args.clerkId,
+      email: args.email,
+      name: args.name,
+      role: args.role,
+      createdAt: Date.now(),
+    });
   },
 });
